bc9eae7404283fb32e17e635829aaa5b3daa96d4
Ayush GUpta <146961251+indayushg@users.noreply.github.com>
2025-04-06 15:06:49 +0000

    conversion

---
+++ 01_basics]/04_conversion.js
@@ -0,0 +1,17 @@
+let score ="22asd"
+// console.log(typeof score)
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+let isLoggedIn =1;
+let boolenisLoggedIn = Boolean(isLoggedIn)
+// console.log(boolenisLoggedIn);
+let someNumber =33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
