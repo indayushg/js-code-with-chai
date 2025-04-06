5756de706aefb206ac87516f6fa696b275caf896
Ayush GUpta <146961251+indayushg@users.noreply.github.com>
2025-04-06 14:37:04 +0000

    data type

---
+++ 01_basics]/02_variable.js
@@ -0,0 +1,7 @@
+const accountId = 45465
+let accountEmail ="ayushgupta@.com"
+var accountpassword ="123"
+// console.log(accountEmail)s
+console.table([accountEmail,accountId,accountpassword])
+console.table([accountEmail,accountId,accountpassword   ])
+console.log([accountEmail,accountId,accountpassword]);
---
+++ 01_basics]/03_dataType.js
@@ -0,0 +1,9 @@
+let name ="ayush gupta"
+let age =18
+let isLoggedIn =false
+
+// console.log(name,age,isLoggedIn);
+
+console.log(typeof  "ayush");
+console.log(typeof  null);
+console.log(typeof  undefined);
